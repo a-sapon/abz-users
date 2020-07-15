1a01cea27dc1191f8401088d753b464d03390b4c
a-sapon <sapon.anya@gmail.com>
2020-07-15 16:46:19 +0300

    Updates

---
+++ precache-manifest.76bece8cac21798bce6de4fb28ab0904.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4c1c620c6a8b6ca85de524cb6ebc93",
+    "url": "/abz-users/index.html"
+  },
+  {
+    "revision": "1b9ad55e0f250d072117",
+    "url": "/abz-users/static/css/main.eb076258.chunk.css"
+  },
+  {
+    "revision": "3c23f8cbd1c631d74828",
+    "url": "/abz-users/static/js/2.bc1799c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/abz-users/static/js/2.bc1799c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9ad55e0f250d072117",
+    "url": "/abz-users/static/js/main.3eae048b.chunk.js"
+  },
+  {
+    "revision": "358923a3b48fd1dc2900",
+    "url": "/abz-users/static/js/runtime-main.154f6db7.js"
+  },
+  {
+    "revision": "1449aa86df53129d9960dbc3fd9f6df7",
+    "url": "/abz-users/static/media/banner-desk-w1024.1449aa86.jpg"
+  },
+  {
+    "revision": "15e8095f6b1935b79d174106f9f07f44",
+    "url": "/abz-users/static/media/banner-desk-w1024@2x.15e8095f.jpg"
+  },
+  {
+    "revision": "1682e5af20c976e1dde78aea93e0b56f",
+    "url": "/abz-users/static/media/banner-mob-w360.1682e5af.jpg"
+  },
+  {
+    "revision": "58cbe525df285893d98df5c0ce6d8058",
+    "url": "/abz-users/static/media/banner-mob-w360@2x.58cbe525.jpg"
+  },
+  {
+    "revision": "e272ab30e31fbb8226db48968eaee1d9",
+    "url": "/abz-users/static/media/banner-tablet-w768.e272ab30.jpg"
+  },
+  {
+    "revision": "0109d6098fea4090915259806df9d60e",
+    "url": "/abz-users/static/media/banner-tablet-w768@2x.0109d609.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abz-users/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abz-users/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/abz-users/static/media/menu-icon.6f6bef28.svg"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/abz-users/static/media/open-sans-v17-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/abz-users/static/media/open-sans-v17-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "8f7128145a3c2b83923552a328df7cdc",
+    "url": "/abz-users/static/media/open-sans-v17-latin-600.8f712814.ttf"
+  },
+  {
+    "revision": "ddacaf4a9db58cda27c76ce88c408546",
+    "url": "/abz-users/static/media/open-sans-v17-latin-600.ddacaf4a.eot"
+  },
+  {
+    "revision": "e16f375be3c2a73b58255a02f6d3a9ce",
+    "url": "/abz-users/static/media/open-sans-v17-latin-600.e16f375b.svg"
+  },
+  {
+    "revision": "049a929c5d81988b3ae6d2f985ca7aa5",
+    "url": "/abz-users/static/media/open-sans-v17-latin-regular.049a929c.ttf"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/abz-users/static/media/open-sans-v17-latin-regular.33543c5c.woff2"
+  },
+  {
+    "revision": "5f4d4bc11d64b6cb605b7030c1997270",
+    "url": "/abz-users/static/media/open-sans-v17-latin-regular.5f4d4bc1.eot"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/abz-users/static/media/open-sans-v17-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/abz-users/static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  },
+  {
+    "revision": "3a41f082d05e2a2756c67f67f21b2324",
+    "url": "/abz-users/static/media/photo-cover.3a41f082.svg"
+  }
+]);
